497a4d5a742fc8b825410d4d369bd59011753236
nrht <i.norihi.472@gmail.com>
2021-03-04 22:31:12 +0900

    implemented blog api

--- opt/blog/main.py
+++ opt/blog/main.py
@@ -1,23 +1,12 @@
-from fastapi import FastAPI, Depends
-from . import schemas, models
-from .database import engine, SessionLocal
-from sqlalchemy.orm import Session
+from fastapi import FastAPI
+from . import  models
+from .database import engine
+from  .routers import blog, user, authentication
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
-def get_db():
-   db = SessionLocal()
-   try:
-      yield db
-   finally:
-      db.close()
-
-@app.post('/blog')
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
-   new_blog = models.Blog(title=request.title, body=request.body)
-   db.add(new_blog)
-   db.commit()
-   db.refresh(new_blog)
-   return new_blog
+app.include_router(authentication.router)
+app.include_router(blog.router)
+app.include_router(user.router)
